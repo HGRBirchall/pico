c90e4e9aace35bc8de4666d8c7b891bfbf38cd5d
HGRBirchall <57440762+HGRBirchall@users.noreply.github.com>
2021-02-25 08:39:34 +0000

    Update soil.py
    add for loop, this will be used to iterate for sensor in sensors and should once completed remove around 40 lines of code.

--- soil.py
+++ soil.py
@@ -17,6 +17,9 @@ led_satisfied = machine.Pin(7, machine.Pin.OUT)
 
 button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
+#number of attached sensors
+sensors = 3
+
 # Make sure that LEDs and relays are not running. This prevents accidental watering.
 pump0.value(1)
 led_pump0.value(0)
@@ -106,9 +109,9 @@ if button.irq(trigger=machine.Pin.IRQ_RISING, handler=start) is True:
     trigger1 = trigger
     trigger2 = trigger
 
-# set up data logging to /data/data_logger(x).txt
+# set up data logging to /data/data_logger(x).txt the .txt will be added automatically
 j = 1
-file_name = "/data/soil_logger"+str(j)+".txt"
+file_name = "/data/soil_logger"+str(j)+
 
 NewFile(file_name, j)
 
@@ -128,79 +131,84 @@ while True:
 # Analogue pins read ~20% when no sensor is plugged in.
 # To prevent accidental watering when unplugged a low of 35% and high of 99% has been set.
 
-    reading0 = round(sensor_soil0.read_u16()*conversion_factor,1)
-    soil0 = trigger0 - reading0
-    if reading0 >=99 or reading0<=35:
-        reading0 = "N/A"
-    else:
-        if soil0 >= 0:
-            led_pump0.value(0)
-            led_satisfied.value(0)
-            for i in range (8):
-                led_satisfied.toggle()
-                led_pump0.toggle()
-                utime.sleep_ms(250)
-            pump0.value(1)
-            utime.sleep(wait)
-        if soil0 < 0:
-            led_pump0.value(1)
-            led_satisfied.value(0)
-            pump0.value(0)
-            for i in range (20):
-                led_pump0.toggle()
-                utime.sleep_ms(250)
-            pump0.value(1)
-            utime.sleep(wait)
+    for sensor in range (sensors):
+        
+        
+        reading0 = round(sensor_soil0.read_u16()*conversion_factor,1)
+        soil0 = trigger0 - reading0
+        if reading0 >=99 or reading0<=35:
+            reading0 = "N/A"
+        else:
+            if soil0 >= 0:
+                led_pump0.value(0)
+                led_satisfied.value(0)
+                for i in range (8):
+                    led_satisfied.toggle()
+                    led_pump0.toggle()
+                    utime.sleep_ms(250)
+                pump0.value(1)
+                utime.sleep(wait)
+            if soil0 < 0:
+                led_pump0.value(1)
+                led_satisfied.value(0)
+                pump0.value(0)
+                for i in range (20):
+                    led_pump0.toggle()
+                    utime.sleep_ms(250)
+                pump0.value(1)
+                utime.sleep(wait)
         
 # Check soil1 and run if soil is dryer than initial reading 
-    reading1 = round(sensor_soil1.read_u16()*conversion_factor,1)
-    soil1 = trigger1 - reading1
-    if reading1 >=99 or reading1<=35:
-        reading1 = "N/A"
-    else:
-        if soil1 >= 0:
-            led_pump1.value(0)
-            led_satisfied.value(0)
-            for i in range (8):
-                led_satisfied.toggle()
-                led_pump1.toggle()
-                utime.sleep_ms(250)
-            pump1.value(1)
-            utime.sleep(wait)
-        if soil1 < 0:
-            led_pump1.value(1)
-            led_satisfied.value(0)
-            pump1.value(0)
-            for i in range (20):
-                led_pump1.toggle()
-                utime.sleep_ms(250)
-            pump1.value(1)
-            utime.sleep(wait)
+        reading1 = round(sensor_soil1.read_u16()*conversion_factor,1)
+        soil1 = trigger1 - reading1
+        if reading1 >=99 or reading1<=35:
+            reading1 = "N/A"
+        else:
+            if soil1 >= 0:
+                led_pump1.value(0)
+                led_satisfied.value(0)
+                for i in range (8):
+                    led_satisfied.toggle()
+                    led_pump1.toggle()
+                    utime.sleep_ms(250)
+                pump1.value(1)
+                utime.sleep(wait)
+            if soil1 < 0:
+                led_pump1.value(1)
+                led_satisfied.value(0)
+                pump1.value(0)
+                for i in range (20):
+                    led_pump1.toggle()
+                    utime.sleep_ms(250)
+                pump1.value(1)
+                utime.sleep(wait)
     
-# Check soil1 and run if soil is dryer than initial reading 
-    reading2 = round(sensor_soil2.read_u16()*conversion_factor,1)
-    soil2 = trigger2 - reading2
-    if reading2 >=99 or reading2<=35:
-        reading2 = "N/A"
-    else:
-        if soil2 >= 0:
-            led_pump2.value(0)
-            led_satisfied.value(0)
-            for i in range (8):
-                led_satisfied.toggle()
-                led_pump2.toggle()
-                utime.sleep_ms(250)
-            pump2.value(1)
-            utime.sleep(wait)
-        if soil2 < 0:
-            led_pump2.value(1)
-            led_satisfied.value(0)
-            pump2.value(0)
-            for i in range (20):
-                led_pump2.toggle()
-                utime.sleep_ms(250)
-            pump2.value(1)
-            utime.sleep(wait)
+# Check soil2 and run if soil is dryer than initial reading 
+        reading2 = round(sensor_soil2.read_u16()*conversion_factor,1)
+        soil2 = trigger2 - reading2
+        if reading2 >=99 or reading2<=35:
+            reading2 = "N/A"
+        else:
+            if soil2 >= 0:
+                led_pump2.value(0)
+                led_satisfied.value(0)
+                for i in range (8):
+                    led_satisfied.toggle()
+                    led_pump2.toggle()
+                    utime.sleep_ms(250)
+                pump2.value(1)
+                utime.sleep(wait)
+            if soil2 < 0:
+                led_pump2.value(1)
+                led_satisfied.value(0)
+                pump2.value(0)
+                for i in range (20):
+                    led_pump2.toggle()
+                    utime.sleep_ms(250)
+                pump2.value(1)
+                utime.sleep(wait)
+            
+# create the data output for the sensor run and log to file.
     log = reading0, trigger0, reading1, trigger1, reading2, trigger2
     file.write(str(log)+"\n")
     file.flush
